07390e0ea1d6aead7a3db3379436e58955152697
To-nevim <114871514+To-nevim@users.noreply.github.com>
2022-11-16 00:04:38 +0100

    base setup

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+import pygame as pg
+from settings import *
+from level import Level
+
+pg.init()
+screen = pg.display.set_mode((screen_width,screen_height))
+clock = pg.time.Clock()
+level = Level(level_map,screen)
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
+    
+    screen.fill('green')
+    level.run()
+    pg.display.update()
+    clock.tick(60)
